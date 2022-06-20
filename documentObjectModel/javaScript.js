c3fab993f53d5daa8ded50e7ac78c2ab2289d99f
Githmin Jayawardhana <githmin21@gmail.com>
2022-06-20 16:04:04 +0530

    h3 blue text

--- documentObjectModel/javaScript.js
+++ documentObjectModel/javaScript.js
@@ -1,6 +1,14 @@
-const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
+const container = document.querySelector('#container');
 
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
-  });
+const content = document.createElement('p');
+content.classList.add('content');
+content.textContent = 'Hey Im Red!';
+content.style.cssText = 'color:blue;';
+container.appendChild(content);
+
+
+const hthree = document.createElement('h3');
+hthree.classList.add('blueh3');
+hthree.textContent = 'Im a blue h3';
+hthree.setAttribute('style' , 'color:blue;');
+container.appendChild(hthree);
