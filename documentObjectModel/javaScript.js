b42e0cac1a1d73ffa8dd648e8055d3829bff10d4
Githmin Jayawardhana <githmin21@gmail.com>
2022-06-21 08:35:58 +0530

    Addded event listner for button 3

--- documentObjectModel/javaScript.js
+++ documentObjectModel/javaScript.js
@@ -28,3 +28,8 @@ container.appendChild(divTwo);
 
 const btnOne = document.querySelector('#btnOne');
 btnOne.onclick = () => alert("Hello World");
+
+const btnTwo = document.querySelector('#btnTwo');
+btnTwo.addEventListener('click', () => {
+  alert("Hello World");
+});
